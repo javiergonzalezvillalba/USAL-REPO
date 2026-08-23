1eb4e1bed4d0639693006d66c671494fae13ed8a
OSCAR JAVIER GONZÁLEZ VILLALBA <oscarjavier.gonzalezvillalba@usal.edu.ar>
2026-08-23 15:22:38 -0300

    actualizacion de ejercicios

---
+++ PROGRAMACION 1/P1 FUNCIONES/gonzalezJaviP1_e16.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Javi on 23/08/2026.
+//
+
+#include "gonzalezJaviP1_e16.h"
+#include <stdio.h>
+#define FILS 5
+#define COLS 5
+
+int sumar(int matriz[][COLS]) {
+    int sum=0;
+
+    for (int i=0; i<FILS; i++) {
+        for (int j=0; j<COLS; j++) {
+            sum=sum+matriz[i][j];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int matriz[FILS][COLS]={41, 15, 8, 99, 23, 7, 56, 12, 4, 88, 31, 64, 2, 9, 73, 18, 45, 33, 91, 5, 67, 21, 50, 11, 39}, suma=0, resultado=0;
+    printf("Suma de los numeros de una matriz.\n");
+    resultado=sumar(matriz);
+    printf("la suma es: %d\n", resultado);
+
+    return 0;
+}
---
+++ PROGRAMACION 1/P1 FUNCIONES/gonzalezJaviP1_e16.h
@@ -0,0 +1,13 @@
+//
+// Created by javig on 23/08/2026.
+//
+
+#ifndef USAL_REPO_GONZALEZJAVIP1_E16_H
+#define USAL_REPO_GONZALEZJAVIP1_E16_H
+
+
+class gonzalezJaviP1_e16 {
+};
+
+
+#endif //USAL_REPO_GONZALEZJAVIP1_E16_H
